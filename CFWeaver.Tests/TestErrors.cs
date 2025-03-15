24ea14338677146dee8cc0da37dff46f99e06909
IanWold <ilwold@hotmail.com>
2025-03-15 10:18:47 -0500

    Add all errors tests

--- CFWeaver.Tests/TestErrors.cs
+++ CFWeaver.Tests/TestErrors.cs
@@ -16,7 +16,142 @@ public class TestErrors
             )
         );
 
+        Assert.Contains("Error 1", console);
         Assert.Contains("Input contains no operation definitions", console);
         Assert.Empty(files);
     }
+    
+    [Fact]
+    public async Task OperationContainsNoStepDefinitions()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                Hello, World!
+                """
+            )
+        );
+
+        Assert.Contains("Error 101", console);
+        Assert.Contains("Operation contains no step definitions", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task OperationHasNoName()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                #   
+
+                * Step : Result
+                """
+            )
+        );
+
+        Assert.Contains("Error 102", console);
+        Assert.Contains("Operation has no name", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task StepDefinitionMustIncludeColon()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                * Step Result
+                """
+            )
+        );
+
+        Assert.Contains("Error 201", console);
+        Assert.Contains("Step definitions must include a single `:` separating the step name from the step results", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task StepHasNoName()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                * : Result
+                """
+            )
+        );
+
+        Assert.Contains("Error 202", console);
+        Assert.Contains("Step has no name", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task StepNamesMustNotContainSpaces()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                * First Step: Result
+                """
+            )
+        );
+
+        Assert.Contains("Error 203", console);
+        Assert.Contains("Step names must not contain spaces", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task ResultHasNoName()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                * Step: Success | | Failure
+                """
+            )
+        );
+
+        Assert.Contains("Error 301", console);
+        Assert.Contains("Result has no name", console);
+        Assert.Empty(files);
+    }
+    
+    [Fact]
+    public async Task ResultHasNoValue()
+    {
+        var (console, files) = await RunTestAsync("input.md -o output.html",
+            (
+                "input.md",
+                """
+                # Operation 1
+
+                * Step: Result =
+                """
+            )
+        );
+
+        Assert.Contains("Error 302", console);
+        Assert.Contains("No return or goto value given for result", console);
+        Assert.Empty(files);
+    }
+    
 }
